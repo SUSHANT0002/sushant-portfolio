08396fc878a8b408d9427f7025ffba8cc37c507d
SUSHANT0002 <sushant.mahadwad@gmail.com>
2023-12-19 17:34:19 +0530

    Download resume link added

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,9 @@
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
-import LogoS from '../../assets/images/ganesha.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import LogoS from '../../assets/images/ganesha.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope, faHome, faLink, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faDownload, faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 const Sidebar = () =>(
 <div className='nav-bar'>
@@ -25,6 +25,9 @@ const Sidebar = () =>(
         <NavLink exact='true' activeclassname="active" className="contact-link" to='/contact'>
             <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
         </NavLink>
+
+        <a className="resume" target='_blank' href='https://drive.google.com/file/d/1L7yFsFOjv3l2xoA6KctO9lZ7osnxigy7/view?usp=sharing'><FontAwesomeIcon icon={faDownload}/></a>
+
     </nav>
     <ul>
         <li>
